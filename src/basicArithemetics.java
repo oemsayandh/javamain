db5b9c8eefb69d778c2b055cfd09d2efaa2147bc
zaid <biggtera@gmail.com>
2026-05-28 13:20:00 +0530

    base arithmetics

---
+++ src/basicArithemetics.java
@@ -0,0 +1,32 @@
+public class basicArithemetics {
+    public static void main(String[] args){
+        int A = 10;
+        int B = 20;
+        int z = 0;
+        // order of working is always """P_E_D_M_A_S"""{parentheses,exponent,division,multiplication,addition,substraction}
+
+        //arithmetics
+        //z = A + B            //addition
+        //z = A - B            //subsctraction
+        //z = A * B            //Multiplication
+        //z = A / B            //Division
+        //z = A % B            // mode (reminder ) odd even useful
+
+        //Augmented assignment operator {use this for low memory usage}
+        // A = A + B -----> A += B
+        // A = A - B -----> A -= B
+        // A = A * B -----> A *= B
+        // A = A / B -----> A /= B
+        // A = A % B -----> A %= B
+
+        // increment decrement
+        // z = A++ (A+1)
+        // z = A-- (A-1)
+        //A++ (increment)
+        //A-- (decrement)
+        //{use for loops a lot }
+
+
+        System.out.println( z );
+    }
+}
