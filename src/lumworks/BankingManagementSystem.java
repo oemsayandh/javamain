fd0ac1c72d34165a947b5f31550ffa9ce540f31e
zaid <biggtera@gmail.com>
2026-07-03 08:12:52 +0530

    managment systems

---
+++ src/lumworks/BankingManagementSystem.java
@@ -0,0 +1,16 @@
+package lumworks;
+import java.util.Scanner;
+
+
+public class EmpManagmentSystem {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int EmpID;
+        String EmpName;
+        int  EmpSalary;
+        
+
+
+    }
+
+}
